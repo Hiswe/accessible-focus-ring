02a73cd072eb76dab968ca973eb24df6d57bafbc
Hiswe <hiswehalya@gmail.com>
2018-10-10 12:46:30 +0700

    DEMO – update

--- src-demo/on-tab.js
+++ src-demo/on-tab.js
@@ -1,5 +1,5 @@
 export default function onTab() {
   console.log(`tabbed!`)
-  const domCheckWrapper = document.querySelector('.js-tab')
+  const domCheckWrapper = document.querySelector(`.js-tab`)
   domCheckWrapper.innerHTML = `tabbed: re-enable focus ring`
 }
