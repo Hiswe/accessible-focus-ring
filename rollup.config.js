e8b81c79201a4ca3ab0fd583c0217e7c9cc85bfc
Hiswe <hiswehalya@gmail.com>
2018-10-10 09:24:07 +0700

    Typescript (#1)
    * LIBRARY – convert to typescript
    
    * LIBRARY – generate typescript definition files

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import merge from 'lodash.merge'
 import { uglify } from 'rollup-plugin-uglify'
+import typescript from 'rollup-plugin-typescript2'
 
 const NAME = `focus-ring`
 
 const baseConfig = {
-  input: `src/index.js`,
+  input: `src/focus-ring.ts`,
   output: {
     format: `umd`,
     name: `focusRing`,
@@ -15,13 +16,14 @@ const npmConfig = merge({}, baseConfig, {
   output: {
     file: `dist/${NAME}.js`,
   },
+  plugins: [typescript()],
 })
 
 const minConfig = merge({}, baseConfig, {
   output: {
     file: `dist/${NAME}.min.js`,
   },
-  plugins: [uglify()],
+  plugins: [typescript(), uglify()],
 })
 
 export default [npmConfig, minConfig]
